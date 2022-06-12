a7f33be8b23696392e46ec13e30e3537f1a899bb
Matty Armendariz <mattyarmendariz@gmail.com>
2022-06-11 23:17:31 -0600

    about me section was made but not yet linked

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
